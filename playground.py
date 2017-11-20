9a152328c5f7af77f06541fdeffd51479817f86a
Alicia Daza <amdazacastillo@gmail.com>
2017-11-21 00:44:44 +0100

    Lists and access to positions

--- playground.py
+++ playground.py
@@ -1,4 +1,24 @@
-## Hello world
+# Hello world
 
 print('Hello world')
 
+# Lists
+fruits = ['Apple', 'Orange', 'Watermelon']
+print(fruits[1])
+print(fruits[-1])
+
+numbers = [0,1,2,3,4,5,6,7,8,9]
+print('from 2 to 6 -> 2:7')
+print(numbers[2:7])
+
+print('greater than 4 -> 5:')
+print(numbers[5:])
+
+print('odd positions -> ::2')
+print(numbers[::2])
+
+print('even positions -> 1::2')
+print(numbers[1::2])
+
+print('4 in numbers')
+print(4 in numbers)
